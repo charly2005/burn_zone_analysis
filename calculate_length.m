29a02e871c8b985975e9d6799f713ec8ed5a28f3
Charles Yao <cy2005932@gmail.com>
2024-05-03 21:57:24 -0700

    Some reason cc2bw can't take in cc

---
+++ calculate_length.m
@@ -0,0 +1,22 @@
+function [l, s] = calculate_length(frame, raw_image_array, bayer_pattern)
+    demosaiced_image = demosaic(raw_image_array(:,:,frame), bayer_pattern);
+    % gain = 5;
+    % color_image = uint8(double(demosaiced_image)*(255/4095)*gain);
+    
+    binary_image = imbinarize(rgb2gray(demosaiced_image), 0.0039);
+    % imshow(binary_image);
+    % hold on
+
+    cc = bwconncomp(binary_image);
+    s = regionprops("table", cc, "Area");
+    area = s.Area;
+    [~,idx] = sort(area,"descend");
+    idx = double(idx);
+    display(cc);
+    imshow(cc2bw(cc));
+    hold on;
+    filtered_binary_image = cc2bw(cc, ObjectsToKeep =idx(1));
+    imshow(filtered_binary_image);
+    hold on
+    l = 1;
+    
---
+++ findlength.m
@@ -0,0 +1,48 @@
+%% clear
+clear        % Clears the workspace 
+clc          % Clears the console window 
+close all    % Closes all figures 
+
+%% Import data from video file 
+%  Our video files are in a *.cine file format which is a binary encoded
+%  file type. We already have a way to extract all the information out of
+%  it, but please take a look at it. 
+
+addpath("External Functions\")
+addpath("Test Data\")
+
+filename = "Test Data\run000_5L_30_2000fps_490us_dec1_trimmed.cine";
+
+% The most important variable that is returned here is the raw_image_array.
+% This is the "raw" image from the camera. For now, I'll add a way for you
+% to convert it to a conventional three color image. The other variables
+% are metadata about the video file. These are structure files, kind of
+% like dictionaries from Python. Properties can be acessed with typical
+% object oriented programming. 
+
+[header, bitmap, setup, raw_image_array] = automated_MatCine(filename);
+
+% Video Size Parameters 
+im_height = size(raw_image_array, 1);
+im_width = size(raw_image_array, 2);
+num_images = size(raw_image_array, 3);
+
+% Time in seconds [s] t = (# of frames)/(Frames/Second) 
+time_s = (0:1:num_images-1)./(setup.FrameRate);
+
+% bayer pattern
+bayer_pattern = "gbrg";
+
+
+%% Test
+clc
+
+
+frame = 30;
+[l, s] = get_length(frame,raw_image_array,bayer_pattern);
+
+
+
+
+
+
