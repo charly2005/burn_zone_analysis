274516cd53bace8a66ac1f64c1cdf2ae6d592ff8
Charles Yao <cy2005932@gmail.com>
2024-05-03 23:45:26 -0700

    region,  cc2bw needs matlab2024

--- calculate_length.m
+++ calculate_length.m
@@ -1,21 +1,19 @@
 function [l, s] = calculate_length(frame, raw_image_array, bayer_pattern)
     demosaiced_image = demosaic(raw_image_array(:,:,frame), bayer_pattern);
-    % gain = 5;
-    % color_image = uint8(double(demosaiced_image)*(255/4095)*gain);
+    gain = 5;
+    color_image = uint8(double(demosaiced_image)*(255/4095)*gain);
     
     binary_image = imbinarize(rgb2gray(demosaiced_image), 0.0039);
     % imshow(binary_image);
     % hold on
 
     cc = bwconncomp(binary_image);
-    s = regionprops("table", cc, "Area");
-    area = s.Area;
-    [~,idx] = sort(area,"descend");
-    idx = double(idx);
-    display(cc);
-    imshow(cc2bw(cc));
+    s = regionprops("table",cc, "Area");
+    [~,idx] = sort(s.Area,"descend");
+    % display(cc);
+    % imshow(cc2bw(cc));
     hold on;
-    filtered_binary_image = cc2bw(cc, ObjectsToKeep =idx(1));
+    filtered_binary_image = cc2bw(cc, ObjectsToKeep=idx(1));
     imshow(filtered_binary_image);
     hold on
     l = 1;
