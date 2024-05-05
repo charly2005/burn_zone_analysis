bfc9a6ce35293562ea57812783e62a65a1841e59
Charles Yao <cy2005932@gmail.com>
2024-05-04 19:52:01 -0700

    work on traversing

--- calculate_length.m
+++ calculate_length.m
@@ -1,9 +1,9 @@
-function [l, s, filtered_binary_image, g] = calculate_length(frame, raw_image_array, bayer_pattern, scale_factor)
+function [l, g] = calculate_length(frame, raw_image_array, bayer_pattern, scale_factor)
 
     demosaiced_image = demosaic(raw_image_array(:,:,frame), bayer_pattern);
     binary_image = imbinarize(rgb2gray(demosaiced_image), 0.0039);
     binary_image = imresize(binary_image, scale_factor);
-
+    
     % bw to cc then cc to bw
     cc = bwconncomp(binary_image);
     s = regionprops("table",cc, "Area");
@@ -11,13 +11,12 @@ function [l, s, filtered_binary_image, g] = calculate_length(frame, raw_image_ar
     filtered_binary_image = cc2bw(cc, ObjectsToKeep=idx(1));
     imshow(filtered_binary_image);
     hold on
-
+    plot(1,1,'ro', 'MarkerSize',5);
     % there are some black dots in the region
     % such as near the bottom of frame 20
     
     
     % move in a 3 x 3 grid
-
     l = 1;
     g = cell(3,3);
     max_h = size(filtered_binary_image, 1);
@@ -29,9 +28,19 @@ function [l, s, filtered_binary_image, g] = calculate_length(frame, raw_image_ar
     curr_y = 1;
     g{2,2} = [curr_x, curr_y];
 
-    not_done = true;
+    done = false;
 
-    while not_done
+
+    % example grid
+    %       1           2         3
+    % 1 [654,798]	[655,798] [656,798]
+    % 2 [654,799]	[655,799] [656,799]
+    % 3 [654,800]	[655,800] [656,800]
+
+    disp(max_h)
+    disp(max_w)
+
+    while true
         % build grid
         for i = 1:3
             % 1 = down, 2 = mid, 3 = top
@@ -42,16 +51,39 @@ function [l, s, filtered_binary_image, g] = calculate_length(frame, raw_image_ar
                  x = curr_x + x_shift;
                  y = curr_y + y_shift;
                  g{i,j} = [x,y];
-                 if x >= max_w || y>= max_h
-                     not_done = false;
+                 disp(x)
+                 disp(y)
+                 if x >= max_w || y>= max_h || x <1
+                     done = true;
                  end
-                 % disp(g{i,j});
+                 % disp(g{i,j}(1));
             end
         end
+
+        if done == true
+            break;
+        end
+
         % implement traversing
+        % go clockwise starting right
+        last_pos = [curr_x, curr_y];
+
+        if filtered_binary_image(g{3,2}(1),g{3,2}(2)) == 1
+            % left
+            curr_x = curr_x - 1;
+        elseif filtered_binary_image(g{3,3}(1),g{3,3}(2)) == 1
+            curr_x = curr_x -1;
+            curr_y = curr_y + 1;
+        elseif filtered_binary_image(g{2,3}(1),g{2,3}(2)) == 1
+        elseif filtered_binary_image(g{1,3}(1),g{1,3}(2)) == 1
+        elseif filtered_binary_image(g{1,2}(1),g{1,2}(2)) == 1
+        elseif filtered_binary_image(g{1,1}(1),g{1,1}(1)) == 1
+        elseif filtered_binary_image(g{})
+        end
         curr_y = curr_y + 1;
-        curr_x = curr_x + 1;
+        % curr_x = curr_x + 1;
         
+
         l = l+1;
     end
 
