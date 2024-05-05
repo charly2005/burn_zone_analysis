21bdd933c8e620f2e29aed94f196af62b729abe3
Charles Yao <cy2005932@gmail.com>
2024-05-04 18:20:35 -0700

    added scale factor and fixed infinite loop

--- calculate_length.m
+++ calculate_length.m
@@ -1,19 +1,13 @@
-function [l, s] = calculate_length(frame, raw_image_array, bayer_pattern)
+function [l, s, filtered_binary_image, g] = calculate_length(frame, raw_image_array, bayer_pattern, scale_factor)
+
     demosaiced_image = demosaic(raw_image_array(:,:,frame), bayer_pattern);
-    % gain = 5;
-    % color_image = uint8(double(demosaiced_image)*(255/4095)*gain);
-    
     binary_image = imbinarize(rgb2gray(demosaiced_image), 0.0039);
-    % imshow(binary_image);
-    % hold on
+    binary_image = imresize(binary_image, scale_factor);
 
     % bw to cc then cc to bw
     cc = bwconncomp(binary_image);
     s = regionprops("table",cc, "Area");
     [~,idx] = sort(s.Area,"descend");
-    % display(cc);
-    % imshow(cc2bw(cc));
-    hold on;
     filtered_binary_image = cc2bw(cc, ObjectsToKeep=idx(1));
     imshow(filtered_binary_image);
     hold on
@@ -26,16 +20,18 @@ function [l, s] = calculate_length(frame, raw_image_array, bayer_pattern)
 
     l = 1;
     g = cell(3,3);
-    max_h = size(raw_image_array, 1);
-    max_w = size(raw_image_array, 2);
+    max_h = size(filtered_binary_image, 1);
+    max_w = size(filtered_binary_image, 2);
     row = filtered_binary_image(1,:,1);
     
     % start at top and move down
     curr_x = find(row,1,'last');
     curr_y = 1;
-    g(2,2) = [curr_x, curr_y];
+    g{2,2} = [curr_x, curr_y];
 
-    while true
+    not_done = true;
+
+    while not_done
         % build grid
         for i = 1:3
             % 1 = down, 2 = mid, 3 = top
@@ -44,19 +40,19 @@ function [l, s] = calculate_length(frame, raw_image_array, bayer_pattern)
                 % 1 = left, 2 = mid, 3 = right
                  x_shift = j-2;
                  x = curr_x + x_shift;
-                 y = curr_x + y_shift;
-                 g(i,j) = [x,y];
-                 
+                 y = curr_y + y_shift;
+                 g{i,j} = [x,y];
+                 if x >= max_w || y>= max_h
+                     not_done = false;
+                 end
+                 % disp(g{i,j});
             end
         end
-
         % implement traversing
+        curr_y = curr_y + 1;
+        curr_x = curr_x + 1;
         
-
-        % break if reach end
-        if g{3,2}(1) == 0
-            break;
-        end
-
         l = l+1;
     end
+
+    l = l * 1.7 / scale_factor;
